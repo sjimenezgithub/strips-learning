9024f95b158128a40bf07743c8277db48ac2d102
Diego Aineto <dieaigar@dsic.upv.es>
2018-01-07 10:42:50 +0100

    updated generate_table_3.sh

--- experiments/ijcai18/generate_table_3.sh
+++ experiments/ijcai18/generate_table_3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-declare -a arr=("blocks" "driverlog" "ferry" "floortile" "grid" "gripper" "hanoi" "hiking" "parking" "satellite" "sokoban" "transport" "zenotravel")
+declare -a arr=("blocks" "driverlog" "ferry" "floortile" "grid" "gripper" "hanoi" "hiking" "npuzzle" "parking" "satellite" "sokoban" "transport" "zenotravel")
 
 ulimit -t 1000
 
 for i in "${arr[@]}"
 do
-   ../../src/compiler.py -s ../../benchmarks/icaps18/$i/ tim_invariants test plan 3
+   ../../src/compiler.py -s ../../benchmarks/ijcai18/labels/$i/ empty_invariants_domain test plan 3
 
    echo $i 
-   ../../src/evaluator2.py -r ../../benchmarks/icaps18/$i/full_domain.pddl learned_domain.pddl ../../benchmarks/icaps18/$i/test-1.pddl
+   ../../src/evaluator2.py -r ../../benchmarks/ijcai18/labels/$i/full_domain.pddl learned_domain.pddl ../../benchmarks/ijcai18/labels/$i/test-01.pddl
    cat planner_out.log | grep -e 'actions\sin'
    cat planner_out.log | grep -e 'total\stime'
 done
